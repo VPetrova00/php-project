e4c467624c28b6acf6c926c98ca955f588c886ef
Ivana Dragneva <ivanadragneva@gmail.com>
2022-06-26 23:19:48 +0300

    login validation and check with database

--- models/Session.php
+++ models/Session.php
@@ -18,12 +18,11 @@ class Session
         return $_SESSION;
     }
 
-    public static function logUser(string $username, string $email, string $password): bool {
+    public static function logUser(string $username, string $password): bool {
         $_SESSION['user_id'] = 5;
 //        self::$session_id++;
 
         $_SESSION['username'] = $username;
-        $_SESSION['email'] = $email;
         $_SESSION['password'] = $password;
 
         return true;
